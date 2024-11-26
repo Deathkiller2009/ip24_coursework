530a4a901d070990e32102594dd7d7bae1370c70
Deathkiller2009 <popovartem439@gmail.com>
2024-11-26 18:36:20 +0300

    Коммит

--- src/main/java/ru/deathkiller2009/ip24_coursework/Ip24CourseworkApplication.java
+++ src/main/java/ru/deathkiller2009/ip24_coursework/Ip24CourseworkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ip24CourseworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ip24CourseworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ip24CourseworkApplication.class, args);
+    }
 
 }
