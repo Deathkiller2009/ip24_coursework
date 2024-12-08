44b590442d88f7d74bbf4e2da675cba2b00522f5
Artyom Popov <popovartem439@gmail.com>
2024-12-08 21:14:37 +0300

    Добавлен модуль для REST сервиса

--- schedule-service/src/main/java/ru/deathkiller2009/ScheduleServiceApplication.java
+++ schedule-service/src/main/java/ru/deathkiller2009/ScheduleServiceApplication.java
@@ -1,10 +1,13 @@
 package ru.deathkiller2009;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ScheduleServiceApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ScheduleServiceApplication.class, args);
     }
 
 }
